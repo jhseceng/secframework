01c671573176956ad9e450d8a3ac506761dfacf7
jharris <justin.r.harris@hotmail.co.uk>
2020-04-16 16:00:17 +0100

    Pass public ip and alb.dns to container

--- scripts/initialize_attacker.sh
+++ scripts/initialize_attacker.sh
@@ -1,14 +1,32 @@
 #!/bin/bash
-apt-get update
+#
+# Example initialize_attacker -v VICTIM
+#
+while getopts 'v:' OPTION;
+do
+  case "$OPTION" in
+    v)
+      VICTIM="$OPTARG"
+      ;;
+
+    *) echo "usage: $0 [-v]" >&2
+       exit 1 ;;
+  esac
+done
+
+ATTACKER=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
 apt-get update
 apt install docker.io python3-pip build-essential libssl-dev libffi-dev -y --force-yes
 pip3 install docker-compose
-cd /var/tmp
+cd /var/tmp || exit 1
 echo "version: '3'" > docker-compose.yml
 echo "services:" >> docker-compose.yml
 echo "  attacker:" >> docker-compose.yml
-echo "    image: franklinjff/attacker:latest" >> docker-compose.yml
+echo "    image: jharris10/attacker" >> docker-compose.yml
+echo "    environment:" >> docker-compose.yml
+echo "      ATTACKER=$ATTACKER" >> docker-compose.yml
+echo "      VICTIM=$VICTIM">> docker-compose.yml
 echo "    ports:" >> docker-compose.yml
 echo "      - \"443:443\"" >> docker-compose.yml
 echo "      - \"5000:5000\"" >> docker-compose.yml
-docker-compose up -d
+#docker-compose up -d
