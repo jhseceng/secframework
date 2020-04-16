8e5227d1a63da1ecf75c56288ab3f20f8cde8ec7
jharris <justin.r.harris@hotmail.co.uk>
2020-04-16 18:15:35 +0100

    Pass S3 bucket filename and CID to instance

--- scripts/initialize_attacker.sh
+++ scripts/initialize_attacker.sh
@@ -15,18 +15,19 @@ do
 done
 
 ATTACKER=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
+echo "$ATTACKER"
 apt-get update
 apt install docker.io python3-pip build-essential libssl-dev libffi-dev -y --force-yes
 pip3 install docker-compose
-cd /var/tmp || exit 1
+cd /var/tmp
 echo "version: '3'" > docker-compose.yml
 echo "services:" >> docker-compose.yml
 echo "  attacker:" >> docker-compose.yml
 echo "    image: jharris10/attacker" >> docker-compose.yml
 echo "    environment:" >> docker-compose.yml
-echo "      ATTACKER=$ATTACKER" >> docker-compose.yml
-echo "      VICTIM=$VICTIM">> docker-compose.yml
+echo "      - ATTACKER=$ATTACKER" >> docker-compose.yml
+echo "      - VICTIM=$VICTIM">> docker-compose.yml
 echo "    ports:" >> docker-compose.yml
 echo "      - \"443:443\"" >> docker-compose.yml
 echo "      - \"5000:5000\"" >> docker-compose.yml
-#docker-compose up -d
+docker-compose up -d
