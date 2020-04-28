df44ad993243f8cb0761addb3eaee4a5fa28db5e
jharris <justin.r.harris@hotmail.co.uk>
2020-04-28 10:26:02 +0100

    Pass S3 bucket filename and CID to instance

--- scripts/initialize_webserver_aws_ami.sh
+++ scripts/initialize_webserver_aws_ami.sh
@@ -9,9 +9,9 @@ aws configure set region $REGION
 #installing from current directory 
 sudo yum update -y
 sudo yum install -y python3-pip python3 python3-setuptools build-essential libssl-dev libffi-dev git
-sudo yum install -y docker
+sudo amazon-linux-extras install docker
+sudo service docker start
 sudo usermod -a -G docker ec2-user
-sudo chkconfig docker on
 sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
