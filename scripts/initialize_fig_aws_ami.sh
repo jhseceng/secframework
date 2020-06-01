49bd123a1eb88176d879de24087c63ef784bc113
jharris <justin.r.harris@hotmail.co.uk>
2020-06-02 00:49:40 +0100

    Updated Webserver to run Amazon EC2 v2 Linux

--- scripts/initialize_fig_aws_ami.sh
+++ scripts/initialize_fig_aws_ami.sh
@@ -18,7 +18,7 @@ FIG=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
 echo "$FIG"
 sudo yum update -y
 sudo yum install -y python3-pip python3 python3-setuptools build-essential libssl-dev libffi-dev git
-sudo amazon-linux-extras install docker
+sudo amazon-linux-extras install docker -y
 sudo service docker start
 sudo usermod -a -G docker ec2-user
 sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
@@ -28,7 +28,7 @@ cd /var/tmp
 echo "version: '3'" > docker-compose.yml
 echo "services:" >> docker-compose.yml
 echo "  fig:" >> docker-compose.yml
-echo "    image: jharris10/securityhubconnector:dev" >> docker-compose.yml
+echo "    image: jharris10/fig:dev" >> docker-compose.yml
 echo "    deploy:">> docker-compose.yml
 echo "      restart_policy:" >> docker-compose.yml
 echo "        condition: unless-stopped" >> docker-compose.yml
