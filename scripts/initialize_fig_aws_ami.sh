17727efb380c404b8c10ce1b1fcd85200df77c5b
jharris <justin.r.harris@hotmail.co.uk>
2020-06-05 22:49:10 +0100

    Updated Webserver to run Amazon EC2 v2 Linux

--- scripts/initialize_fig_aws_ami.sh
+++ scripts/initialize_fig_aws_ami.sh
@@ -2,6 +2,7 @@
 #
 # Example initialize_fig -v VICTIM
 #
+DOCKER_VERSION=18.09.2
 while getopts 'r:' OPTION;
 do
   case "$OPTION" in
@@ -18,8 +19,28 @@ FIG=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
 echo "$FIG"
 sudo yum update -y
 sudo yum install -y python3-pip python3 python3-setuptools build-essential libssl-dev libffi-dev git
-sudo amazon-linux-extras install docker -y
-sudo service docker start
+#sudo amazon-linux-extras install docker -y
+#sudo service docker start
+
+INSTALL_DOCKER="true"
+if [[ "$INSTALL_DOCKER" == "true" ]]; then
+    sudo amazon-linux-extras enable docker
+    sudo groupadd -fog 1950 docker && sudo useradd --gid 1950 docker
+    sudo yum install -y docker
+    sudo usermod -aG docker $USER
+
+    # Remove all options from sysconfig docker.
+    sudo sed -i '/OPTIONS/d' /etc/sysconfig/docker
+
+    sudo mkdir -p /etc/docker
+    sudo mv $TEMPLATE_DIR/docker-daemon.json /etc/docker/daemon.json
+    sudo chown root:root /etc/docker/daemon.json
+
+    # Enable docker daemon to start on boot.
+    sudo systemctl daemon-reload
+    sudo systemctl enable docker
+fi
+
 sudo usermod -a -G docker ec2-user
 sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
